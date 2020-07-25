9b51f363db61563cfb171e00262f1606b4b83f82
1nd1v1dual <63320261+1nd1v1dual@users.noreply.github.com>
2020-07-25 13:29:46 +0300

    Update Phone.java
    Дописанная версия

--- Phone/src/RodFill/Phone.java
+++ Phone/src/RodFill/Phone.java
@@ -1,29 +1,69 @@
 package RodFill;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Phone {
+public class Phone implements Closeable {
 
     private final Socket socket;
     private final BufferedReader reader;
     private final BufferedWriter writer;
 
-    public Phone(Socket socket) {
-        this.socket =socket;
-        this.reader = createReader(this.socket.getInputStream());
-    }
-    public Phone(ServerSocket socket){
-        this.socket = server.accept();
+    public Phone(String ip, int port) {
+        try{
+            this.socket = new Socket(ip,port);
+            this.reader = createReader();
+            this.writer = createWriter();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    private BufferedReader createReader(InputStream input){
-        return new BufferedReader(new InputStreamReader(input));
+    public Phone(ServerSocket server){
+        try{
+            this.socket = server.accept();
+            this.reader = createReader();
+            this.writer = createWriter();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    private BufferedWriter createWriter(OutputSteam output){
-        return new BuffereedWriter(new OutputStreamWriter(output));
+    public void writeLine(String message){
+        try{
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readLine(){
+        try{
+            return reader.readLine();
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BufferedReader createReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    private BufferedWriter createWriter() throws IOException {
+            return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.close();
+        reader.close();
+        socket.close();
     }
 }
