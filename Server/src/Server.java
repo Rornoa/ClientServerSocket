fcfe2595f5c5dff7f86c9f719a692a7ad5559ac1
1nd1v1dual <63320261+1nd1v1dual@users.noreply.github.com>
2020-07-25 13:33:02 +0300

    Update Server.java
    Многопоточный сервер на Phone

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,6 +1,7 @@
+import RodFill.Phone;
+
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
 
@@ -10,32 +11,26 @@ public class Server {
         {
             System.out.println("Server started!");
 
-            while(true)
-            try (
-                Socket socket = server.accept();
-                BufferedWriter writer =
-                        new BufferedWriter(
-                                new OutputStreamWriter(
-                                        socket.getOutputStream()));
-                BufferedReader reader =
-                        new BufferedReader(
-                                new InputStreamReader(
-                                        socket.getInputStream()));
-            ) {
-                String request = reader.readLine();
-                System.out.println("Request: " + request);
-                String response = (int) (Math.random() * 30 -10) + "";
-                System.out.println("Response: " + response);
-                writer.write(response);
-                writer.newLine();
-                writer.flush();
-            } catch (NullPointerException e)
-            {
-                e.printStackTrace();
+            while(true) {
+                Phone phone = new Phone(server);
+                new Thread(() -> {
+                    String request = phone.readLine();
+                    System.out.println("Request: " + request);
+                    String response = (int) (Math.random() * 30 - 10) + "";
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                    }
+                    phone.writeLine(response);
+                    System.out.println("Response: " + response);
+                    try {
+                        phone.close();
+                    } catch (IOException e) {
+                    }
+                }).start();
             }
 
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
